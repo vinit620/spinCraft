911735da62bdfe8fed83bd84a94f62f7c8e22d10
Vinit Jha <jhavinit01@gmail.com>
2023-08-11 19:12:07 +0530

    Initial Commit

---
+++ spinCraft/spinCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  spinCraftApp.swift
+//  spinCraft
+//
+//  Created by Vinit Jha on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct spinCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
